6aca89ec4b46e4d1a9f0697e1ada6dd16fd4ab0a
kalmanp <hvub,ihvub,i123>
2017-02-11 20:05:40 +0200

    fsdds

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 8080;
 
 
-server.listen(port, function () {
-    console.log('Server listening at port %d', port);
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
 });
 
-// Routing
-app.use(express.static(__dirname + '/public'));
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+        console.log(msg);
+    });
+});
 
 // Chatroom
 
@@ -71,4 +75,8 @@ io.on('connection', function (socket) {
             });
         }
     });
+});
+
+http.listen(port, function(){
+    console.log('listening on' + port);
 });
