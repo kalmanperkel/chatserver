15d06441240326c5dde1c3c37bd639104d528538
kalmanp <hvub,ihvub,i123>
2017-02-11 15:45:25 +0200

    fsdds

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 8080;
+
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -13,6 +15,6 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(3000, function(){
+http.listen(port, function(){
     console.log('listening on *:3000');
 });
